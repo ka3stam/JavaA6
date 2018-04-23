62dd3038cfe42471711aefa734f1c8e5b720d814
stamatiki <37833897+ka3stam@users.noreply.github.com>
2018-04-23 19:30:02 +0300

    Update JavaA6.java

--- src/javaa6/JavaA6.java
+++ src/javaa6/JavaA6.java
@@ -3,11 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package javaa6;
 
 import javax.swing.JOptionPane;
-import javax.swing.*; 
+import javax.swing.*;
 
 /**
  *
@@ -20,27 +19,25 @@ public class JavaA6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         String in;
-         int m=0, p;
-        char ch='a';
-        in=JOptionPane.showInputDialog("Δώσατε ένα αλφαριθμητικό: ");
-        in=in.toLowerCase().trim(); 
-        for (int i=0;i<in.length();i++) 
-        {
-            if (in.charAt(i)>ch) 
-                  ch=in.charAt(i);
-        }
-        for (int i=0;i<in.length();i++) 
-            {
-              if (in.charAt(i)==ch)
-                 {
-                    ch=in.charAt(i);
-                    m=m+1;
-                 }
+        int m = 0, p;
+        char ch = 'a';
+        in = JOptionPane.showInputDialog("Enter an alphanumeric:");
+        in = in.toLowerCase().trim();
+        for (int i = 0; i < in.length(); i++) {
+            if (in.charAt(i) > ch) {
+                ch = in.charAt(i);
             }
-        p=in.indexOf("ch")+2;
-    System.out.println("Το μεγαλύτερο γράμμα είναι το: " + ch + ",εμφανίζεται " + m + " φορές και η πρώτη του εμφάνιση είναι στη θέση " + p );
-    System.exit(0);
-    }   
+        }
+        for (int i = 0; i < in.length(); i++) {
+            if (in.charAt(i) == ch) {
+                ch = in.charAt(i);
+                m = m + 1;
+            }
+        }
+        p = in.indexOf("ch") + 2;
+        System.out.println("The biggest letter is: " + ch + ", appeared " + m + " times and it's first position is: " + p);
+        System.exit(0);
+    }
 }
